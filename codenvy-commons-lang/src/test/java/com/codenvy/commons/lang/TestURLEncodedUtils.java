2a3faf8b548d7cac1959db33a97aeabb2a1deb41
Alexander Garagatyi <agaragatyi@codenvy.com>
2013-08-06 11:18:42 +0300

    CLDIDE-962: change source header

--- codenvy-commons-lang/src/test/java/com/codenvy/commons/lang/TestURLEncodedUtils.java
+++ codenvy-commons-lang/src/test/java/com/codenvy/commons/lang/TestURLEncodedUtils.java
@@ -1,32 +1,21 @@
 /*
- * ====================================================================
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * CODENVY CONFIDENTIAL
+ * __________________
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *  [2012] - [2013] Codenvy, S.A.
+ *  All Rights Reserved.
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
  */
-
 package com.codenvy.commons.lang;
-
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -35,7 +24,6 @@ import java.net.URI;
 import java.util.*;
 
 import static org.testng.Assert.*;
-
 
 public class TestURLEncodedUtils {
 
