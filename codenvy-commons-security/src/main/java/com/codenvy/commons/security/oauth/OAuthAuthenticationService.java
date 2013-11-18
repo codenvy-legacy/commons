59b85eeccba795940a56cabee89c7bb8705a24e9
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2013-11-18 17:36:31 +0200

    CLDIDE-1361: added possibility to use POJO object of url info for OAuth 1.0

--- codenvy-commons-security/src/main/java/com/codenvy/commons/security/oauth/OAuthAuthenticationService.java
+++ codenvy-commons-security/src/main/java/com/codenvy/commons/security/oauth/OAuthAuthenticationService.java
@@ -24,12 +24,7 @@ import com.codenvy.commons.security.shared.Token;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -221,6 +216,7 @@ public class OAuthAuthenticationService {
     }
 
     @POST
+    @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     @Path("token")
     public Token token(@QueryParam("oauth_provider") String oauth_provider, @QueryParam("userId") String userId,
